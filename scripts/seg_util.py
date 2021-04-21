cdff2c048118bdd526b618bdf1ddad584fd96d83
Noe Guisset <noe.guisset@gmail.com>
2021-04-21 10:26:08 +0100

    added segmentation engine

---
+++ scripts/seg_util.py
@@ -0,0 +1,56 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+import torch
+from PIL import Image
+from pathlib import Path
+#weight file hosted at https://f000.backblazeb2.com/file/fypLanding/Unet-Mobilenet.pt
+
+class SegmentationEngine():
+    def __init__(self, pathWeights):
+        self.net=torch.load(pathWeights)
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    def inferImage(self,img, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
+        self.net.eval()
+        t = T.Compose([T.ToTensor(), T.Normalize(mean, std)])
+        image = t(img)
+        self.net.to(self.device); image=image.to(self.device)
+        with torch.no_grad():
+            image = image.unsqueeze(0)        
+            output = self.net(image)
+            mask = torch.argmax(output, dim=1)
+            mask = mask.cpu().squeeze(0)
+        return mask
+
+    def maskToPIL(self,mask):
+        maskNp=mask.numpy()
+        img = Image.fromarray(np.uint8(maskNp) , 'L')   
+        return img
+
+if __name__ == '__main__':
+
+    import glob
+    IMAGE_DIR = '/ext_files/segmentation-trained/testSequence/seq1/*.jpg'
+    pathWeights=''
+    seq=glob.glob(IMAGE_DIR)
+    segEngine=SegmentationEngine(pathWeights)
+    
+    for file in seq:
+        i=3
+        img = Image.open(file)
+        file_name=Path(file).stem
+        img = img.resize((1152, 768), Image.ANTIALIAS)
+        pred_mask=segEngine.inferImage(img)
+        fig, (ax1, ax2) = plt.subplots(1,2, figsize=(20,10))
+        ax1.imshow(img)
+        ax1.set_title('Picture');
+        ax2.imshow(pred_mask)
+        ax2.set_title('Prediction')
+        ax2.set_axis_off()
+        pred_mask.numpy()
+        fig.savefig('/ext_files/segmentation-trained/testSequence/seq1/figs/'+file_name+'_fig.png')
+        xxx=pred_mask.numpy()
+        img = Image.fromarray(np.uint8(xxx) , 'L')
+        dir_mask='/ext_files/segmentation-trained/testSequence/seq1/masks/'+file_name+'_mask.jpg'
+        img.save(dir_mask)
+
