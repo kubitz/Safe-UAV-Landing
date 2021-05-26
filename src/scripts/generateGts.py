b2a850da02101ff4eae6cb5b8cd7acf7e55ee691
Noe Guisset <noe.guisset@gmail.com>
2021-05-26 11:13:22 +0100

    formatting

--- src/scripts/generateGts.py
+++ src/scripts/generateGts.py
@@ -12,10 +12,10 @@ import pandas as pd
 from safelanding.config import *
 from safelanding.labels import *
 from safelanding.metrics import LzGtGenerator
+from tqdm import tqdm
 
 
-
-HEADLESS = False
+HEADLESS = True
 
 basePath = Path(__file__).parents[2]
 dataPath = basePath.joinpath("data", "imgs")
@@ -28,46 +28,46 @@ for dir in resultPath.iterdir():
 resultList.sort()
 resultList.pop(0)
 
-seq = resultList[5]
-pathImgsGt = str(dataPath.joinpath(seq.stem, "gts", "*"))
-pathImgs = str(dataPath.joinpath(seq.stem, "images", "*"))
+for seq in tqdm(resultList):
+    pathImgsGt = str(dataPath.joinpath(seq.stem, "gts", "*"))
+    pathImgs = str(dataPath.joinpath(seq.stem, "images", "*"))
 
-config = configparser.ConfigParser()
-config.read(str(dataPath.joinpath(seq.stem, "config.ini")))
-dataset = config["SETTINGS"]["dataset"]
-labels = datasetLabels[dataset]
-r_landing = config["SETTINGS"]["r_landing"]
+    config = configparser.ConfigParser()
+    config.read(str(dataPath.joinpath(seq.stem, "config.ini")))
+    dataset = config["SETTINGS"]["dataset"]
+    labels = datasetLabels[dataset]
+    r_landing = config["SETTINGS"]["r_landing"]
 
-gtsSeg = glob.glob(pathImgsGt)
-rgbImgs = glob.glob(pathImgs)
-gtsSeg.sort()
-rgbImgs.sort()
-df_lzs = pd.read_csv(
-    str(resultPath.joinpath(seq.stem, "results_lzs.csv")),
-    converters={"position": ast.literal_eval, 'id': lambda x: str(x)},
-)
-df_lzs['id'] = df_lzs['id'].astype(str)
-nbInferredImgs = df_lzs["id"].nunique()
+    gtsSeg = glob.glob(pathImgsGt)
+    rgbImgs = glob.glob(pathImgs)
+    gtsSeg.sort()
+    rgbImgs.sort()
+    df_lzs = pd.read_csv(
+        str(resultPath.joinpath(seq.stem, "results_lzs.csv")),
+        converters={"position": ast.literal_eval, 'id': lambda x: str(x)},
+    )
+    df_lzs['id'] = df_lzs['id'].astype(str)
+    nbInferredImgs = df_lzs["id"].nunique()
 
-lzsGts = []
+    lzsGts = []
 
-if not (len(gtsSeg) == nbInferredImgs == len(rgbImgs)):
-    raise IndexError("Non-matching number of ground truths/landing zones")
+    if not (len(gtsSeg) == nbInferredImgs == len(rgbImgs)):
+        raise IndexError("Non-matching number of ground truths/landing zones")
 
-for idx, gt in enumerate(gtsSeg):
-    lzs = df_lzs[df_lzs["id"] == Path(gt).stem].to_dict("records")
-    gtImg = cv.imread(gt)
-    for lz in lzs:
-        lz["gt"], lz["reasons"] = LzGtGenerator.getLzGt(gtImg, lz, labels)
-    lzsGts += lzs
+    for idx, gt in enumerate(gtsSeg):
+        lzs = df_lzs[df_lzs["id"] == Path(gt).stem].to_dict("records")
+        gtImg = cv.imread(gt)
+        for lz in lzs:
+            lz["gt"], lz["reasons"] = LzGtGenerator.getLzGt(gtImg, lz, labels)
+        lzsGts += lzs
 
-    if not HEADLESS:
-        imgPath = rgbImgs[idx]
-        img = cv.imread(imgPath)
-        img = LzGtGenerator.draw_gt_lzs(img, lzs)
-        cv.imshow("gt", img)
-        cv.waitKey(0)
-        cv.destroyAllWindows()
+        if not HEADLESS:
+            imgPath = rgbImgs[idx]
+            img = cv.imread(imgPath)
+            img = LzGtGenerator.draw_gt_lzs(img, lzs)
+            cv.imshow("gt", img)
+            cv.waitKey(0)
+            cv.destroyAllWindows()
 
-dfLzsGt = pd.DataFrame(lzsGts)
-dfLzsGt.to_csv(resultPath.joinpath(seq.stem, "gt_lzs.csv"), index=False)
+    dfLzsGt = pd.DataFrame(lzsGts)
+    dfLzsGt.to_csv(resultPath.joinpath(seq.stem, "gt_lzs.csv"), index=False)
